3567b60bb3457dbd673ed060e462bfd71658d9b1
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2021-04-29 11:37:46 +0100

    data model implemented

--- project1/js/script.js
+++ project1/js/script.js
@@ -4,3 +4,4 @@ const mymap = L.map('mapid').fitWorld();
 //add the tile layer on the class, in this case i used thunderforest
 L.tileLayer('https://tile.thunderforest.com/neighbourhood/{z}/{x}/{y}.png?apikey=6d9911c3c07e404eaa8dd1c1067b8c7e', {
 }).addTo(mymap);
+
