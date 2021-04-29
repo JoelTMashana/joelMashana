cda49f0849196b86b478ee644b675029eb87363f
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2021-04-29 16:29:33 +0100

    easy buttons implemented

--- project1/js/script.js
+++ project1/js/script.js
@@ -4,4 +4,20 @@ const mymap = L.map('mapid').fitWorld();
 //add the tile layer on the class, in this case i used thunderforest
 L.tileLayer('https://tile.thunderforest.com/neighbourhood/{z}/{x}/{y}.png?apikey=6d9911c3c07e404eaa8dd1c1067b8c7e', {
 }).addTo(mymap);
+ 
 
+//buttons
+//modal pop up button
+L.easyButton('<ion-icon name="information-outline"></ion-icon>', function(btn, map){
+    $('#myModal').modal('show');
+}).addTo(mymap);
+
+//retrieve location
+L.easyButton('<ion-icon name="compass-outline"></ion-icon>', function(btn, map){
+    
+}).addTo(mymap);
+//set marker
+L.easyButton('<ion-icon name="location-outline"></ion-icon>', function(btn, map){
+    
+}).addTo(mymap);
+//set night mode
