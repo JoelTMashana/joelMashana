689d08f827c9e06941ae640d829c47f0674b8946
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2021-05-14 15:44:34 +0100

    opencage

--- project1/php/getUserLocationCountryInfoData.php
+++ project1/php/getUserLocationCountryInfoData.php
@@ -1,12 +1,13 @@
 <?php
-	// remove for production
+    // remove for production
 	ini_set('display_errors', 'On');
 	error_reporting(E_ALL);
 	$executionStartTime = microtime(true);
-
-
+    include 'getUserLocationOpenCageData.php';
+    
+    
 	// these params are taken from the data part of the ajax call
-	$url='http://api.openweathermap.org/data/2.5/onecall?lat=' . $_POST['latitude'] . '&lon=' . $_POST['longitude'] . '&exclude=minutely,hourly,current' . '&appid=d24dbe12c720afa869a1120f204c6c05';
+	$url='https://restcountries.eu/rest/v2/name/' . $countryName . '?fullText=true';
 
 	//curls obj initialised and stored in var
 	// the API I use will speicfy what I need to copy
@@ -16,22 +17,24 @@
 	curl_setopt($ch, CURLOPT_URL, $url);
 	 
 	//executed the curl obj and store it in a var
-	$countryInfoDecode =curl_exec($ch);
+	$countryInfo =curl_exec($ch);
 	curl_close($ch);
     //converts the json encoded string and turns into pphp var
 	// true ensures that JSON is returned as associative array
-	$countryInfoDecode  = json_decode($countryInfoDecode ,true);	
+	$countryInfo  = json_decode($countryInfo ,true);	
     // this is the strucure of the output
 	// status and data
 	$output['status']['code'] = "200";
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $countryInfoDecode['daily'];
+
+	$output['data'] = $countryInfo[0];
 
 	header('Content-Type: application/json; charset=UTF-8');
     
 	// echo prints the output via the json_encode func
 	// which returns a string containing the JSON representation of the supplied value
-	echo json_encode($output); 
-?>
+	echo json_encode($output);
+    
+    ?>
--- project1/php/getUserLocationOpenCageData.php
+++ project1/php/getUserLocationOpenCageData.php
@@ -4,9 +4,9 @@
 	error_reporting(E_ALL);
 	$executionStartTime = microtime(true);
 
-
+   
 	// these params are taken from the data part of the ajax call
-	$url='http://api.openweathermap.org/data/2.5/onecall?lat=' . $_POST['latitude'] . '&lon=' . $_POST['longitude'] . '&exclude=minutely,hourly,current' . '&appid=d24dbe12c720afa869a1120f204c6c05';
+	$url='https://api.opencagedata.com/geocode/v1/json?q=' . $_POST['latitude']. ',' . $_POST['longitude'] . '&key=ee756e3c37284a8db239a456ef2be343' . '&pretty=1';
 
 	//curls obj initialised and stored in var
 	// the API I use will speicfy what I need to copy
@@ -22,12 +22,21 @@
 	// true ensures that JSON is returned as associative array
 	$countryInfoDecode  = json_decode($countryInfoDecode ,true);	
     // this is the strucure of the output
+
+	
+	//vars for other apis
+	$currencySymbol= $countryInfoDecode['results']['annotations']['currency']['symbol'];
+	$countryName = $countryInfoDecode['results'][0]['components']['country'];
+	$countryCode = $countryInfoDecode['results'][0]['components']['country_code'];
+
+
 	// status and data
 	$output['status']['code'] = "200";
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $countryInfoDecode['daily'];
+	$output['data'] = $countryInfoDecode['results'];
+
 
 	header('Content-Type: application/json; charset=UTF-8');
     
