d7b3be8ebcaf4eecfba29a9137900db6b52fb4b1
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2021-05-15 21:49:26 +0100

    reverting to commit 8b88a28

---
+++ project1/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-new_branch',
+    'version' => 'dev-new_branch',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e842000682be4d4d99319f7e243a343b8c6972de',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-new_branch',
+      'version' => 'dev-new_branch',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e842000682be4d4d99319f7e243a343b8c6972de',
+    ),
+    'opencage/geocode' => 
+    array (
+      'pretty_version' => 'v3.0.1',
+      'version' => '3.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9b926dd46a0bb7896af3f7c8f933f95a5a4647a4',
+    ),
+  ),
+);
