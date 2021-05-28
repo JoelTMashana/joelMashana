cae3b3113e3ca796b90ce283353a786974f1a540
JoelTMashana <67970779+JoelTMashana@users.noreply.github.com>
2021-05-28 15:38:19 +0100

    extra images delted

---
+++ project1/libs/php/getLocationOpenCageDataForwardGeo.php
@@ -0,0 +1,41 @@
+<?php
+	// remove for production
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);
+	$executionStartTime = microtime(true);
+
+   //for forward geocoding
+	// these params are taken from the data part of the ajax call
+	$url='https://api.opencagedata.com/geocode/v1/json?q=' . urlencode($_POST['placename']) . '&key=ee756e3c37284a8db239a456ef2be343' . '&pretty=1';
+
+	//curls obj initialised and stored in var
+	// the API I use will speicfy what I need to copy
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL, $url);
+	 
+	//executed the curl obj and store it in a var
+	$countryInfoDecode =curl_exec($ch);
+	curl_close($ch);
+    //converts the json encoded string and turns into pphp var
+	// true ensures that JSON is returned as associative array
+	$countryInfoDecode  = json_decode($countryInfoDecode ,true);	
+    // this is the strucure of the output
+
+
+	// status and data
+	$output['status']['code'] = "200";
+	$output['status']['name'] = "ok";
+	$output['status']['description'] = "success";
+	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+	$output['status'] = $url;
+	$output['data'] = $countryInfoDecode['results'];
+
+
+	header('Content-Type: application/json; charset=UTF-8');
+    
+	// echo prints the output via the json_encode func
+	// which returns a string containing the JSON representation of the supplied value
+	echo json_encode($output); 
+?>
---
+++ project1/libs/php/getLocationWikiData.php
@@ -0,0 +1,40 @@
+<?php
+	// remove for production
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);
+	$executionStartTime = microtime(true);
+
+   
+	// these params are taken from the data part of the ajax call
+	$url='api.geonames.org/wikipediaSearchJSON?formatted=true&q=' . $_POST['placename'] . '&maxRows=10' . '&username=joelmashana&style=full';
+
+	//curls obj initialised and stored in var
+	// the API I use will speicfy what I need to copy
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL, $url);
+	 
+	//executed the curl obj and store it in a var
+	$countryInfoDecode =curl_exec($ch);
+	curl_close($ch);
+    //converts the json encoded string and turns into pphp var
+	// true ensures that JSON is returned as associative array
+	$countryInfoDecode  = json_decode($countryInfoDecode ,true);	
+    // this is the strucure of the output
+
+
+	// status and data
+	$output['status']['code'] = "200";
+	$output['status']['name'] = "ok";
+	$output['status']['description'] = "success";
+	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+	$output['data'] = $countryInfoDecode['geonames'];
+
+
+	header('Content-Type: application/json; charset=UTF-8');
+    
+	// echo prints the output via the json_encode func
+	// which returns a string containing the JSON representation of the supplied value
+	echo json_encode($output); 
+?>
